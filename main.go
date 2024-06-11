b84f420d31049194775b0f6cf43843f350bd0661
krls2020 <mail@karelhemza.cz>
2024-06-11 10:03:53 +0200

    w

--- main.go
+++ main.go
@@ -72,16 +72,13 @@ func addEntry(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response := fmt.Sprintf(`<pre>
+	response := fmt.Sprintf(`
 This is a simple, basic GO application running on <a href="https://zerops.io/">Zerops.io</a>.
 Each request adds an entry to the PostgreSQL database and returns a count.
 
 See the source repository (<a href="https://github.com/zeropsio/recipe-go">https://github.com/zeropsio/recipe-go</a>) for more information.
 
-Entry added successfully with random data: %s. Total count: %d
-
-
-</pre>`, randomData, count)
+Entry added successfully with random data: %s. Total count: %d`, randomData, count)
 
     	fmt.Fprint(w, response)
 }
